bb5b950180ce15560fd564f1b297fd8a88aa661f
Sushant Mishra <mishrasushant1901@gmail.com>
2022-01-08 12:59:52 +0530

    Interfaces

---
+++ src/com/company/Solution3.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic1{
+    int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic1 {
+    public int divisorSum(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
+
+class Solution3 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        AdvancedArithmetic1 myCalculator = new Calculator();
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
