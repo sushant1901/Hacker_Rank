a1f6bde5f25111f96c2901ac593438b997fdc3b2
Sushant Mishra <mishrasushant1901@gmail.com>
2022-01-09 12:23:38 +0530

    Pattern Syntax Checker

---
+++ src/com/company/Solution5.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution5
+{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while(testCases>0){
+            String pattern = in.nextLine();
+            //Write your code
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+
+
+}
+
+
