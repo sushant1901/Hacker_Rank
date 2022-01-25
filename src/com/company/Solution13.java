2ab0d316dde2a9b621e836cae84538747c138de2
Sushant Mishra <mishrasushant1901@gmail.com>
2022-01-25 22:30:44 +0530

    Exception string to integer

---
+++ src/com/company/Solution13.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution13 {
+    public static void main(String[] args) throws IOException {
+
+        try(Scanner scan = new Scanner(System.in);){
+            int input = Integer.parseInt(scan.nextLine());
+            System.out.println(input);
+        }
+        catch(NumberFormatException e){
+            System.out.println("Bad String");
+        }
+
+
+    }
+}
