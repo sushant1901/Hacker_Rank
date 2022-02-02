e752ca41ccd39cfe7d5e680d921b88e87178b732
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-02 22:58:10 +0530

    A very Big Sum

---
+++ src/com/company/Solution20.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution20 {
+
+
+    static int solveMeFirst(int a, int b) {
+        return a+b;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+}
