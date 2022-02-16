a928afdb854fede4a6e84b4b8e9c886273b337e7
Sushant Mishra <mishrasushant1901@gmail.com>
2022-02-16 19:24:28 +0530

    Java BigInteger

---
+++ src/com/company/Solution26.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution26 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
