8f4218023354fe94ea51d6a43cc67c8ab30f1432
Sushant Mishra <mishrasushant1901@gmail.com>
2022-04-16 12:35:41 +0530

    Java SHA-256

---
+++ src/com/company/Solution39.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+import java.security.*;
+
+public class Solution39 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner input = new Scanner(System.in);
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
+        m.reset();
+        m.update(input.nextLine().getBytes());
+        for (byte i : m.digest()) {
+            System.out.print(String.format("%02x", i));
+        }
+        System.out.println();
+    }
+}
